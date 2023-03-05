62b900a6300d3f0b63906bbb496eb46cdd64a45b
JongHooon <qnm541@gmail.com>
2023-03-05 22:27:00 +0900

    prog 뒤에 있는 큰 수 찾기 - stack

--- programmers/뒤에 있는 큰 수 찾기/main.swift
+++ programmers/뒤에 있는 큰 수 찾기/main.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> [Int] {
-    var result: [Int] = []
+    var result: [Int] = Array(repeating: -1, count: numbers.count)
+    var stack: [(Int, Int)] = []
     
     for (i, n) in numbers.enumerated() {
-        for j in i..<numbers.count {
-            if numbers[j] > n {
-                result.append(numbers[j])
-                break
+        if !stack.isEmpty {
+            while !stack.isEmpty && stack.last!.1 < n {
+                result[stack.removeLast().0] = n
             }
-            if j == numbers.count - 1 { result.append(-1) }
         }
+        stack.append((i, n))
     }
     return result
 }
