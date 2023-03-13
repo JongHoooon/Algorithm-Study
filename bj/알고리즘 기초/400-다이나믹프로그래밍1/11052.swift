140c41f50839fe592c5a0869ecdb2732bc1697a2
JongHooon <qnm541@gmail.com>
2023-03-14 01:09:44 +0900

    bj 11052

--- bj/알고리즘 기초/400-다이나믹프로그래밍1/11052.swift
+++ bj/알고리즘 기초/400-다이나믹프로그래밍1/11052.swift
@@ -1,11 +1,16 @@
 import Foundation
 
 let N = Int(readLine()!)!
-let cards = readLine()!.components(separatedBy: " ")
+let cards = readLine()!.components(separatedBy: " ").map { Int($0)! }
+var dp = [Int](repeating: 0, count: N+1)
 
+for i in 1..<N+1 { 
+    for j in 1..<i+1 { 
+        dp[i] = max(dp[i], dp[i-j]+cards[j-1])
+    }
+}
 
-
-
+print(dp[N])
 
 
 
@@ -21,4 +26,8 @@ let cards = readLine()!.components(separatedBy: " ")
 2 + 1 + 1: 5 + 1 + 1
 1 + 1 + 1 + 1: 1 + 1 + 1 + 1
 
+1 - 1
+2 - 5
+3 - 6, 6
+4 - 7, 7, 5+5
 */
