bf190151befcf1c8744d36a6c72d3017fc2d936b
JongHooon <qnm541@gmail.com>
2023-03-13 23:49:23 +0900

    bj 11052

--- bj/알고리즘 기초/400-다이나믹프로그래밍1/11052.swift
+++ bj/알고리즘 기초/400-다이나믹프로그래밍1/11052.swift
@@ -1,2 +1,24 @@
 import Foundation
 
+let N = Int(readLine()!)!
+let cards = readLine()!.components(separatedBy: " ")
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+4: 7
+3 + 1: 6 + 1
+2 + 2: 5 + 5
+2 + 1 + 1: 5 + 1 + 1
+1 + 1 + 1 + 1: 1 + 1 + 1 + 1
+
+*/
