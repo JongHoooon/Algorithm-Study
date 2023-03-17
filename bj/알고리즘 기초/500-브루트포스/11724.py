d08c0ef09b9d98c13fc1a0a440649713965fc4eb
JongHooon <qnm541@gmail.com>
2023-03-17 18:11:47 +0900

    bj 11724 (python)

--- bj/알고리즘 기초/500-브루트포스/11724.py
+++ bj/알고리즘 기초/500-브루트포스/11724.py
@@ -1,8 +1,33 @@
 import sys
+from collections import deque
 
 sys.setrecursionlimit(10 ** 9)
 
 N, M = map(int, input().split())
-graph = [[] for _ in range(n+1)]
+graph = [[] for _ in range(N+1)]
+isVisited = [False for _ in range(N+1)]
 
-for _ in range()
+for _ in range(M):
+    n1, n2 = map(int, input().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1)
+    
+def bfs(node):
+    queue = deque([node])
+    isVisited[node] = True
+    
+    while queue:
+        q = queue.popleft()
+        
+        for n in graph[q]:
+            if isVisited[n] == False:
+                queue.append(n)
+                isVisited[n] = True
+                
+count = 0
+for i in range(1, N+1):
+    if not(isVisited[i]):
+        count += 1
+        bfs(i)
+        
+print(count)
