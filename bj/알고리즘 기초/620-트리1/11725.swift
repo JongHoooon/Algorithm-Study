d9985d13e071866822b5e389728595825915c3f9
JongHooon <qnm541@gmail.com>
2023-03-18 22:19:00 +0900

    bj 11725 (swift)

--- bj/알고리즘 기초/620-트리1/11725.swift
+++ bj/알고리즘 기초/620-트리1/11725.swift
@@ -1,17 +1,11 @@
-//
-//  main.swift
-//  큰 수 만들기
-//
-//  Created by JongHoon on 2023/01/18.
-//
-
+// import Foundation
 
 let N = Int(readLine()!)!
 var tree: [[Int]] = Array(repeating: [], count: N+1)
 var parents: [Int] = Array(repeating: 0, count: N+1)
 var isVisited: [Bool] = Array(repeating: false, count: N+1)
 
-for _ in 0..<N-1 {
+for _ in 0..<N-1 { 
     let input = readLine()!
         .split(separator: " ")
         .map { Int($0)! }
@@ -20,15 +14,18 @@ for _ in 0..<N-1 {
     tree[input[1]].append(input[0])
 }
 
-func bfs(node: Int) {
+
+/**
+시간: 468 ms
+*/
+func bfs(node: Int) { 
     isVisited[node] = true
     var queue: [Int] = [node]
 
-    while !queue.isEmpty {
+    while !queue.isEmpty { 
         let n = queue.removeFirst()
-        print(n)
         let next = tree[n]
-        for i in next {
+        for i in next { 
             if isVisited[i] == true { continue }
             isVisited[i] = true
             parents[i] = n
@@ -38,8 +35,26 @@ func bfs(node: Int) {
 }
 
 bfs(node: 1)
-print(tree)
 
-for i in 2..<N+1 {
+
+/**
+시간: 252 ms
+*/
+func dfs(v: Int) { 
+    isVisited[v] = true
+    for node in tree[v] {
+        if isVisited[node] { continue }
+        isVisited[node] = true
+        parents[node] = v 
+        dfs(v: node)
+    }
+}
+
+dfs(v: 1)
+
+for i in 2..<N+1 { 
     print(parents[i])
 }
+
+
+
