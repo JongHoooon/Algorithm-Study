aae8dd0638bcb0ea3173cc7b05d38779b4391fc8
JongHooon <qnm541@gmail.com>
2023-03-21 03:15:46 +0900

    bj 1759 (swift)

--- bj/알고리즘 기초/530-브루트포스-재귀/1759.swift
+++ bj/알고리즘 기초/530-브루트포스-재귀/1759.swift
@@ -5,4 +5,39 @@ let LC: [Int] = readLine()!
 
 let L: Int = LC[0]
 let C: Int = LC[1]
+var characters: [String] = readLine()!
+    .split(separator: " ")
+    .map { String($0) }
+    .sorted()    
 
+
+let vowels: [String] = ["a", "e", "i", "o", "u"]
+var answer: [String] = []
+
+func backTracking(depth: Int, count: Int) { 
+
+    if depth == L {
+        var countOfVowels: Int = 0
+        var countOfConsonant: Int = 0
+        
+        for s in answer { 
+            if vowels.contains(s) { 
+                countOfVowels += 1
+            } else { 
+                countOfConsonant += 1 
+            }
+        }
+        if countOfVowels >= 1 && countOfConsonant >= 2 {
+            print(answer.joined()) 
+        }
+        return
+    }
+
+    for i in count..<C { 
+        answer.append(characters[i])
+        backTracking(depth: depth+1, count: i+1)
+        answer.removeLast()
+    }
+}
+
+backTracking(depth: 0, count: 0)
