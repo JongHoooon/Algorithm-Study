8d454d7e03f56a465658dd5ba25d838f1051ea43
JongHooon <qnm541@gmail.com>
2023-06-05 16:40:25 +0900

    prog K진수에서 소수 개수 구하기

--- Programmers/problems/LV2/K진수에서_소수_개수_구하기.swift
+++ Programmers/problems/LV2/K진수에서_소수_개수_구하기.swift
@@ -1,19 +1,11 @@
-//
-//  main.swift
-//  practice
-//
-//  Created by JongHoon on 2023/06/05.
-//
 
 import Foundation
 
 func solution(_ n:Int, _ k:Int) -> Int {
 
-    let nums = String(n, radix: k)
+    let nums: [Int] = String(n, radix: k)
         .split(separator: "0")
         .map { Int($0)! }
-    
-    print(nums)
 
     let answer = nums
         .filter { isPrime($0) }
@@ -35,6 +27,9 @@ func isPrime(_ num: Int) -> Bool {
     return true
 }
 
+/*
+String -> Int로 바꾸는 과저에서 over flow 발생할 수 있다. 조심
 
-print(solution(110011, 10))
+진법 바꾸는법 체크 
+*/
 
--- Programmers/table.md
+++ Programmers/table.md
@@ -6,3 +6,4 @@
 - [✨ 혼자 놀기의 달인](/Programmers/problems/LV2/혼자_놀기의_달인.swift) - Implementaion, dfs
 - [✨ 양궁 대회](/Programmers/problems/LV2/양궁대회.swift) - DFS, Brute Force
 - [두 큐 합 같게 만들기](/Programmers/problems/LV2/두_큐_합_같게_만들기.swift) - Array, Two Point
+- [✨K진수에서 소수 개수 구하기](/Programmers/problems/LV2/K진수에서_소수_개수_구하기.swift) - Radix OverFlow, Prime
