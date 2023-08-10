01e82f56ec8af1b69a58fff6ccf0ee22a006297d
JongHoon <qnm541@gmail.com>
2023-08-10 13:59:24 +0900

    add 11724_연결요소의개수

--- BaekJoon/Recommand/2_Exhaustive_Search/10448_유레카이론.swift
+++ BaekJoon/Recommand/2_Exhaustive_Search/10448_유레카이론.swift
@@ -1,38 +1,45 @@
-let T = Int(readLine()!)!
-var nums: [Int] = []
-var limitN = 0
-var triangle: [Int] = []
+// let T = Int(readLine()!)!
+// var nums: [Int] = []
+// var limitN = 0
+// var triangle: [Int] = []
 
-for _ in 0..<T  {
-    let n = Int(readLine()!)!
-    nums.append(n)
-}
+// for _ in 0..<T  {
+//     let n = Int(readLine()!)!
+//     nums.append(n)
+// }
 
-let maxN = nums.max()!
+// let maxN = nums.max()!
 
-for n in 1...45 { 
-    if n*(n+1)/2 > maxN { 
-        limitN = n-1
-        break
-    }
+// for n in 1...45 { 
+//     if n*(n+1)/2 > maxN { 
+//         limitN = n-1
+//         break
+//     }
 
-    triangle.append(n*(n+1)/2)
-}
+//     triangle.append(n*(n+1)/2)
+// }
 
-for n in nums {
-    print(eureka(n))
-}
+// for n in nums {
+//     print(eureka(n))
+// }
 
-func eureka(_ n: Int) -> Int { 
-    for i in 0..<limitN { 
-        for j in i..<limitN {
-            for k in j..<limitN { 
-                if triangle[i] + triangle[j] + triangle[k] == n { 
-                    return 1
-                }
-            }
-        }
-    }
+// func eureka(_ n: Int) -> Int { 
+//     for i in 0..<limitN { 
+//         for j in i..<limitN {
+//             for k in j..<limitN { 
+//                 if triangle[i] + triangle[j] + triangle[k] == n { 
+//                     return 1
+//                 }
+//             }
+//         }
+//     }
 
-    return 0
+//     return 0
+// }
+
+let test = "test"
+let chars: [Character] = Array(test)
+
+for c in chars {
+    print(c)
 }
